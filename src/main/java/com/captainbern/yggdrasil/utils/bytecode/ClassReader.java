70a9785c790778a08bd222e5cba785449f8a1fad
CaptainBern <captainberneby@gmail.com>
2014-03-28 14:43:24 +0100

    Ugh...

--- src/main/java/com/captainbern/yggdrasil/utils/bytecode/ClassReader.java
+++ src/main/java/com/captainbern/yggdrasil/utils/bytecode/ClassReader.java
@@ -2,6 +2,10 @@ package com.captainbern.yggdrasil.utils.bytecode;
 
 import com.captainbern.yggdrasil.utils.IOUtils;
 
+import java.io.UnsupportedEncodingException;
+
+import static com.captainbern.yggdrasil.utils.bytecode.Opcode.*;
+
 public class ClassReader {
 
     protected final byte[] bytes;
@@ -12,7 +16,15 @@ public class ClassReader {
 
     protected final int major;
 
-    protected ConstantPool pool;
+    protected int[] items;
+
+    protected String[] strings;
+
+    protected int maxStringLength;
+
+    protected int header;
+
+    private String className;
 
     public ClassReader(final byte[] bytes) {
         this(bytes, 0);
@@ -30,7 +42,71 @@ public class ClassReader {
         this.minor = IOUtils.readShort(bytes, offset + 4);
         this.major = IOUtils.readShort(bytes, offset + 6);
 
-        this.pool = new ConstantPool(bytes);
+        if(major > Opcode.JDK_8) {
+            throw new IllegalArgumentException("Major version too high!");
+        }
+
+        // Define the constant pool
+        items = new int[IOUtils.readUnsignedShort(bytes, offset + 8)];
+        int n = items.length;
+        strings = new String[n];
+        int max = 0;
+        int index = offset + 10;
+        for (int i = 1; i < n; ++i) {
+            items[i] = index + 1;
+            int size;
+            switch (bytes[index]) {
+                case TAG_FIELD:
+                case TAG_METHOD:
+                case TAG_INTERFACE_METHOD:
+                case TAG_INTEGER:
+                case TAG_FLOAT:
+                case TAG_DESCRIPTOR:
+                case TAG_INVOKEDYNAMIC:
+                    size = 5;
+                    break;
+                case TAG_LONG:
+                case TAG_DOUBLE:
+                    size = 9;
+                    ++i;
+                    break;
+                case TAG_UTF_STRING:
+                    size = 3 + IOUtils.readUnsignedShort(bytes, index + 1);
+                    if (size > max) {
+                        max = size;
+                    }
+                    break;
+                case TAG_METHOD_HANDLE:
+                    size = 4;
+                    break;
+                // case ClassWriter.CLASS:
+                // case ClassWriter.STR:
+                // case ClassWriter.MTYPE
+                default:
+                    size = 3;
+                    break;
+            }
+            index += size;
+        }
+        maxStringLength = max;
+        // the class header information starts just after the constant pool
+        header = index;
+
+        // Define the class name
+        int classNameIndex = items[IOUtils.readUnsignedShort(this.bytes, header + 2)];
+        int item = IOUtils.readUnsignedShort(this.bytes, classNameIndex);
+        int stringLength = IOUtils.readUnsignedShort(this.bytes, item);
+        int stringReadIndex = classNameIndex + 2;
+
+        byte[] nameBytes = new byte[stringLength];
+
+        System.arraycopy(this.bytes, stringReadIndex, nameBytes, 0, stringLength);
+
+        try {
+            System.out.println(new String(nameBytes, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     public final byte[] getBytes() {
@@ -49,7 +125,15 @@ public class ClassReader {
         return this.major;
     }
 
-    public ConstantPool getConstantPool() {
-        return this.pool;
+    public int[] getConstantPool() {
+        return this.items;
+    }
+
+    public int readItem(int index) {
+        return getConstantPool()[index];
+    }
+
+    public int getAccess() {
+        return IOUtils.readUnsignedShort(this.bytes, header);
     }
 }
